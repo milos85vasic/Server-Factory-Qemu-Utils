93aae6dcd8a199a3f06075e15a77971da09732c6
Milos Vasic <milos85vasic@gmail.com>
2020-11-02 11:32:03 +0100

    MSF-28 - Working on Qemu scripts.

--- delete_bridge.sh
+++ delete_bridge.sh
@@ -17,9 +17,35 @@ if test -e "$script_path_full"; then
   else
 
     echo "ERROR: $bridge Network bridge was not deleted"
-    exit 1
   fi
 else
 
   echo "No network bridge to delete"
+fi
+
+if_up_script="qemu-ifup"
+if_up_script_full="/etc/$if_up_script"
+if_up_script_backup="/etc/_$if_up_script"
+
+if test -e "$if_up_script_backup"; then
+
+  if test -e "$if_up_script_full"; then
+    if rm -f "$if_up_script_full"; then
+
+      echo "$if_up_script_full Is removed"
+    else
+
+      echo "ERROR: $if_up_script_full was not removed"
+      exit 1
+    fi
+  fi
+
+  if mv "$if_up_script_backup" "$if_up_script_full"; then
+
+    echo "$if_up_script_backup restored to: $if_up_script_full"
+  else
+
+    echo "$if_up_script_backup was not restored to: $if_up_script_full"
+    exit 1
+  fi
 fi
--- machine.sh
+++ machine.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-iso=$2
-machine=$1
-display=$(sh get_display.sh)
-acceleration=$(sh get_acceleration.sh)
-disk=$(sh create_disk.sh "$machine" 20)
-#tapName=$(sh create_and_get_tap.sh)
-#bridgeName=$(sh create_and_get_bridge.sh)
+if_up_script="qemu-ifup"
+if_up_script_full="/etc/$if_up_script"
+if_up_script_backup="/etc/_$if_up_script"
 
-if ! test -e /etc/qemu-ifup && uname | grep -i "darwin" > /dev/null; then
+if test -e "$if_up_script_full" && ! test -e "$if_up_script_backup"; then
+
+  if mv "$if_up_script_full" "$if_up_script_backup"; then
+
+    echo "$if_up_script_full backed up to: $if_up_script_backup"
+  else
+
+    echo "$if_up_script_full was not backed up to: $if_up_script_backup"
+    exit 1
+  fi
+fi
+
+if ! test -e /etc/qemu-ifup; then
 
   echo "qemu-ifup: Scrip version for macOS is missing, it will be installed"
   if sudo cp macOS/qemu-ifup.sh /etc && sudo mv /etc/qemu-ifup.sh /etc/qemu-ifup; then
@@ -21,7 +29,14 @@ if ! test -e /etc/qemu-ifup && uname | grep -i "darwin" > /dev/null; then
   fi
 fi
 
-sh create_network.sh && \
+iso=$2
+machine=$1
+display=$(sh get_display.sh)
+acceleration=$(sh get_acceleration.sh)
+disk=$(sh create_disk.sh "$machine" 20)
+#tapName=$(sh create_and_get_tap.sh)
+#bridgeName=$(sh create_and_get_bridge.sh)
+
 #  ip link set "$tapName" master "$bridgeName"
   sudo qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
     -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
