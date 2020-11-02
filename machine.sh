85731b80c637678f767bb9cf87ce93313358727e
Milos Vasic <milos85vasic@gmail.com>
2020-11-02 12:46:06 +0100

    MSF-28 - Working on Qemu scripts.

--- machine.sh
+++ machine.sh
@@ -41,7 +41,8 @@ disk=$(sh create_disk.sh "$machine" 20)
   sudo qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
     -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
     -drive file="$disk,format=qcow2,if=virtio" \
-    -netdev tap,id=tap1 -device rtl8139,netdev=tap1 \
+    -net nic -net tap,ifname=tap0 \
     -cdrom "$iso"
 
 # -net nic -net tap,ifname=tap0 \
+#-netdev tap,id=tap2 -device rtl8139,netdev=tap2 \
