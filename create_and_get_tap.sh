1137ffa0b6e03df7c7f39ac02a107ffbc2345cbe
Milos Vasic <milos85vasic@gmail.com>
2020-11-03 14:07:43 +0100

    MSF-325 - Dynamic tap obtain - Completed.

--- create_and_get_tap.sh
+++ create_and_get_tap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-# TODO:
-echo "tap0"
+for ITER in 0 .. 100
+do
+
+  tap="tap$ITER"
+  if ! ifconfig "$tap" 2> /dev/null; then
+
+    echo "$tap"
+    exit 0
+  fi
+done
+
+echo "ERROR: Could not obtain tap candidate"
+exit 1
