0b81e342c4c57ddbcc73b33302ea5931277a424f
Milos Vasic <milos85vasic@gmail.com>
2020-11-05 15:22:53 +0100

    MSF-331 - Bind common interfaces.

--- bind_interfaces_to_bridge.sh
+++ bind_interfaces_to_bridge.sh
@@ -2,14 +2,18 @@
 
 tap=$2
 bridgeName=$1
-interface="en0"
+log=$(sh get_machine_log_name.sh "$tap")
 
-if sudo ifconfig "$bridgeName" addm "$interface"; then
+echo "Binding interfaces" | sudo tee -a "$log"
+interfaces="en0;en1;en2;en3;en4;en5;eth0;eth1;eth2;eth3;eth4;eth5"
+export IFS=";"
+for interface in $interfaces; do
 
-  echo "$bridgeName: Bound with: $interface"
-  exit 0
-else
+  if sudo ifconfig "$bridgeName" addm "$interface"; then
 
-  echo "$bridgeName: Failed to bind with: $interface"
-  sh fail_and_cleanup.sh "$tap"
-fi
+      echo "$bridgeName: Bound with $interface" | sudo tee -a "$log"
+    else
+
+      echo "$bridgeName: Not bound with $interface" | sudo tee -a "$log"
+    fi
+done
