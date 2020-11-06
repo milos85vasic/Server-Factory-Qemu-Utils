68bbe5687504812215f7afab953aa7307ac5a73b
Milos Vasic <milos85vasic@gmail.com>
2020-11-06 22:43:20 +0100

    MSF-330 - Properties file(s).

--- run.sh
+++ run.sh
@@ -13,6 +13,16 @@ else
   echo "$image: Is not available"
   echo "$image: Obtaining"
 
+  image_location_properties="image_location.properties"
+  image_provider_properties="image_provider.properties"
+  if test -e "$image_location_properties"; then
+
+    image_location=$(cat "$image_location_properties")
+    echo "Images location: $image_location"
+  else
+
+    echo "image_location.properties: Not available, please create file and add absolute path to images to it"
+  fi
 fi
 
-sh machine.sh "$machine" "$iso"
+# sh machine.sh "$machine" "$iso"
