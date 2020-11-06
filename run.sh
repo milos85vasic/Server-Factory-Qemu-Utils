8537c0fc7e848463eb72cbdb634b9f4ce3f5d009
Milos Vasic <milos85vasic@gmail.com>
2020-11-06 22:34:18 +0100

    MSF-330 - Checking image existence.

--- run.sh
+++ run.sh
@@ -3,4 +3,16 @@
 iso=$2
 machine=$1
 
+echo "Checking system image availability"
+image="$machine/disk.qcow2"
+if test -e "$image"; then
+
+  echo "$image: Is available"
+else
+
+  echo "$image: Is not available"
+  echo "$image: Obtaining"
+
+fi
+
 sh machine.sh "$machine" "$iso"
