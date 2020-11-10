2d48dd9074ca381fa69bbff00eed2829317a0529
Milos Vasic <milos85vasic@gmail.com>
2020-11-10 17:32:23 +0100

    MSF-338 - Keep system awake while running Qemu virtual machine reverted.

--- machine.sh
+++ machine.sh
@@ -10,15 +10,7 @@ display=$(sh get_display.sh)
 acceleration=$(sh get_acceleration.sh)
 disk=$(sh create_disk.sh "$machine" 20 "$tap")
 
-if sh is_macos.sh; then
-
-  sleep_lock="caffeinate"
-else
-
-  sleep_lock="sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target --runtime &&"
-fi
-
-if ! sudo "$sleep_lock" qemu-system-x86_64 -accel "$acceleration" -cpu host -m 3072 -smp 3 \
+if ! sudo qemu-system-x86_64 -accel "$acceleration" -cpu host -m 8192 -smp 4 \
   -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
   -drive file="$disk,format=qcow2,if=virtio" \
   -net nic -net tap,ifname="$tap" \
